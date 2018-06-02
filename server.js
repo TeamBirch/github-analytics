0da4523451aea8bcbc11679e17dc644e186b2726
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-06-02 15:41:55 +0000

    Working OAuth

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const config = require("./config.js") // For the github key and secret
+const path = require('path');
 
 const port = process.env.PORT || 8080;
+
 // ================================================
 // Github OAUTH ROUTES! IMPORTANT!!!
 // ================================================
@@ -10,20 +12,20 @@ const port = process.env.PORT || 8080;
 const githubOAuth = require('github-oauth')({
   githubClient: config.GITHUB_KEY,
   githubSecret: config.GITHUB_SECRET,
-  // baseURL: 'http://localhost:' + port,
+  // baseURL: 'http://localhost:' + port, // For none cloud 9 users
   baseURL: 'https://codesmith-rhythmmagi.c9users.io/',
-  loginURI: '/auth/github',
+  loginURI: '/login',
   callbackURI: '/repos'
 });
 
-app.get("/auth/github", (req, res) => {
+app.get("/login", (req, res) => {
   console.log("started oauth");
   return githubOAuth.login(req, res);
 });
 
 app.get("/repos", (req, res) => {
   console.log("received callback");
-  return githubOAuth.callback(req, res);
+  return githubOAuth.callback(req, res); 
 });
 
 githubOAuth.on('error', (err) => {
@@ -31,7 +33,6 @@ githubOAuth.on('error', (err) => {
 });
 
 githubOAuth.on('token', (token, serverResponse) => {
-  console.log('here is your shiny new github oauth token', token);
   serverResponse.end(JSON.stringify(token));
 });
 
@@ -40,8 +41,7 @@ githubOAuth.on('token', (token, serverResponse) => {
 // TEST ROUTES 
 
 app.get('/', (req, res) => {
-    res.render('./views/test.html')
+    res.sendFile(path.join(__dirname + '/views/test.html'));
 });
-
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
